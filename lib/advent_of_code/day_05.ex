87b5b68dd90f7d4f93750ed849f6d6baf48becfe
Greg Lincoln <greg@greglincoln.com>
2022-12-05 10:58:57 -0500

    Initial day 5!

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,68 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  def part1(input) do
+    {stacks, steps} = parse_input(input)
+
+    Enum.reduce(steps, stacks, &do_step/2) |> to_output()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    {stacks, steps} = parse_input(input)
+
+    Enum.reduce(steps, stacks, &do_step_9001/2) |> to_output()
   end
+
+  defp parse_input(input) do
+    [stacks, steps] =
+      input
+      |> String.split("\n\n")
+      |> Enum.map(&String.split(&1, "\n"))
+
+    {parse_stacks(stacks), parse_steps(steps)}
+  end
+
+  defp to_output(input) do
+    input
+    |> Map.values()
+    |> Enum.map(&hd/1)
+    |> Enum.join()
+  end
+
+  defp do_step([num, _from, _to], stacks) when num == 0, do: stacks
+
+  defp do_step([num, from, to], stacks) do
+    [head | tail] = stacks[from]
+    stacks = Map.update!(stacks, to, &[head | &1]) |> Map.replace!(from, tail)
+    do_step([num - 1, from, to], stacks)
+  end
+
+  defp do_step_9001([num, from, to], stacks) do
+    {to_move, remaining} = stacks[from] |> Enum.split(num)
+    Map.update!(stacks, to, &(to_move ++ &1)) |> Map.replace!(from, remaining)
+  end
+
+  defp parse_steps(steps) do
+    steps |> Enum.filter(&not_empty/1) |> Enum.map(&parse_step/1)
+  end
+
+  defp parse_step(row) do
+    row
+    |> String.split(" ")
+    |> tl()
+    |> Enum.take_every(2)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp parse_stacks(stacks) do
+    number_row = List.last(stacks) |> String.trim()
+    col_count = String.last(number_row) |> String.to_integer()
+    crates = Enum.take(stacks, length(stacks) - 1)
+    for col <- 0..(col_count - 1), into: %{}, do: {col + 1, parse_stack_col(crates, col)}
+  end
+
+  defp parse_stack_col(stacks, col) do
+    stacks = for row <- stacks, do: String.slice(row, col * 4 + 1, 1)
+    stacks |> Enum.filter(&not_empty/1)
+  end
+
+  defp not_empty(str), do: String.trim(str) != ""
 end
