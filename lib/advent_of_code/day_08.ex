cfb8e50132ee4165aa629fb3cea3a98158374d2a
Greg Lincoln <greg@greglincoln.com>
2022-12-08 17:55:22 -0500

    Cleanups.

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -22,14 +22,17 @@ defmodule TreeSizeMap do
     %TreeSizeMap{rows: rows, cols: cols, width: tuple_size(cols), height: height}
   end
 
-  def visible(map, pos)
-      when rem(pos, map.width) == map.width - 1 or div(pos, map.width) == map.width - 1 or
-             rem(pos, map.height) == map.width - 1 or div(pos, map.height) == map.width - 1 or
-             rem(pos, map.height) == 0 or div(pos, map.height) == 0 or
-             rem(pos, map.width) == 0 or div(pos, map.width) == 0,
-      do: {true, 0}
+  def edge_height_cast(map),
+    do: for(pos <- 0..(map.width * map.height - 1), do: edge_height_cast(map, pos))
 
-  def visible(map, pos) do
+  defp edge_height_cast(map, pos)
+       when rem(pos, map.width) == map.width - 1 or div(pos, map.width) == map.width - 1 or
+              rem(pos, map.height) == map.width - 1 or div(pos, map.height) == map.width - 1 or
+              rem(pos, map.height) == 0 or div(pos, map.height) == 0 or
+              rem(pos, map.width) == 0 or div(pos, map.width) == 0,
+       do: {true, 0}
+
+  defp edge_height_cast(map, pos) do
     x = rem(pos, map.width)
     y = div(pos, map.height)
     col = elem(map.cols, x)
@@ -43,39 +46,41 @@ defmodule TreeSizeMap do
       Enum.slice(col, y + 1, map.height - y),
       Enum.slice(col, 0, y) |> Enum.reverse()
     ]
-    |> Enum.map(&tree_visible(&1, size, 0))
+    |> Enum.map(&tree_edge_cast(&1, size, 0))
     |> scenic_score()
   end
 
   defp scenic_score(list),
     do: Enum.reduce(list, fn {v, s}, {av, as} -> {av || v, s * as} end)
 
-  def tree_visible([next | _rest], size, steps) when next >= size, do: {false, steps + 1}
+  defp tree_edge_cast([next | _rest], size, steps) when next >= size,
+    do: {false, steps + 1}
 
-  def tree_visible([next | rest], size, steps) when next < size,
-    do: tree_visible(rest, size, steps + 1)
+  defp tree_edge_cast([], _size, steps), do: {true, steps}
 
-  def tree_visible([], _size, steps), do: {true, steps}
+  defp tree_edge_cast([next | rest], size, steps) when next < size,
+    do: tree_edge_cast(rest, size, steps + 1)
 
   defp get_row(input),
     do: String.split(input, "") |> Enum.filter(&(&1 != "")) |> Enum.map(&String.to_integer/1)
 
-  defp get_col(rows, col), do: for(y <- rows, do: String.slice(y, col, 1) |> String.to_integer())
+  defp get_col(rows, col),
+    do: for(y <- rows, do: String.slice(y, col, 1) |> String.to_integer())
 end
 
 defmodule AdventOfCode.Day08 do
   def part1(input) do
-    map = TreeSizeMap.new(input)
-
-    for(pos <- 0..(map.width * map.height - 1), do: TreeSizeMap.visible(map, pos))
+    input
+    |> TreeSizeMap.new()
+    |> TreeSizeMap.edge_height_cast()
     |> Enum.filter(&elem(&1, 0))
     |> Enum.count()
   end
 
   def part2(input) do
-    map = TreeSizeMap.new(input)
-
-    for(pos <- 0..(map.width * map.height - 1), do: TreeSizeMap.visible(map, pos))
+    input
+    |> TreeSizeMap.new()
+    |> TreeSizeMap.edge_height_cast()
     |> Enum.map(&elem(&1, 1))
     |> Enum.max()
   end
