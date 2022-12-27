6cfab43a443b4628be27fa58946cf370c474d2a0
Greg Lincoln <greg@greglincoln.com>
2022-12-27 07:38:33 -0500

    Add reasonable d18p1.

--- lib/advent_of_code/day_18.ex
+++ lib/advent_of_code/day_18.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Day18 do
-  def part1(_args) do
+  def part1(input) do
+    points =
+      input
+      |> String.split()
+      |> Enum.map(fn pt -> String.split(pt, ",") |> Enum.map(&String.to_integer/1) end)
+
+    points |> Enum.reduce(0, fn pt, a -> a + (6 - Enum.count(points, &is_neighbor(&1, pt))) end)
   end
 
   def part2(_args) do
+  end
+
+  defp is_neighbor(pt1, pt2) do
+    case {pt1, pt2} do
+      {pt, pt} ->
+        false
+
+      {[x1, y1, z1], [x2, y2, z2]}
+      when (x1 == x2 and y1 == y2 and abs(z1 - z2) == 1) or
+             (x1 == x2 and z1 == z2 and abs(y1 - y2) == 1) or
+             (y1 == y2 and z1 == z2 and abs(x1 - x2) == 1) ->
+        true
+
+      _ ->
+        false
+    end
   end
 end
